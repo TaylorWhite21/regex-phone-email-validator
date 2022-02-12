ab7a74656bb243a9b8de1bfb5b1f956254d997ec
Taylor <taylorwhite21@yahoo.com>
2022-02-12 17:51:39 -0500

    completed lab requirements

--- automation/phone-email-validator.py
+++ automation/phone-email-validator.py
@@ -5,12 +5,14 @@ def phone_validator():
   content = open('assets/potential-contacts.txt', 'r').read()
   with open('phone_numbers.txt', 'w+') as f:
     phone_numbers = re.findall(r'[0-9-+x.()]{7,}', content)
-    phone_numbers.sort()
     formatted_numbers = []
     for number in phone_numbers:
       numbers = number.replace(".", "").replace("-", "").replace("(", "").replace(")", "").replace("+", "")
       if len(numbers) > 10:
         formatted_numbers.append(numbers[:3] + "-" + numbers[3:6]+"-" + numbers[6:10])
+      elif len(numbers) < 7:
+        formatted_numbers.append("206" + "-" + numbers[0:3]+"-" + numbers[3:7])
+    formatted_numbers.sort()
     
     for n in formatted_numbers: 
       f.write(n + "\n")
